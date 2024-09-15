0326691b3d1e82b221b956fc9f012cc7febdd55f
skyden <sarada12t@gmail.com>
2024-09-15 08:00:20 +0530

    pushing all

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+function Footer() {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <div className='footer-social-icons'>
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className='footer-content-center'>
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>HOME</li>
+                    <li>ABOUT US</li>
+                    <li>DELIVERY</li>
+                    <li>PRIVACY POLICY</li>
+                </ul>
+            </div>
+            <div className='footer-content-right'>
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>6899699696</li>
+                    <li>tomato@gmail.com</li>
+                </ul>
+            </div>
+            
+        </div>
+        <hr />
+        <p className='footer-copyright'>© 2024. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './LoginPopup.css'
+import { assets } from '../../assets/assets'
+
+function LoginPopup({setShowLogin}) {
+  const [currState , setCurrState] = useState("sign up")
+  return (
+    <div className='login-popup'>
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={()=> setShowLogin(false) } src={assets.cross_icon} alt="" />
+        </div>
+        <div className='login-popup-inputs'>
+          {currState === "Login" ? <></>: <input type="text" placeholder='Your name' required /> }
+         
+          <input type="email" placeholder='Your email' required />
+          <input type="password" placeholder='Password' required/>
+
+        </div>
+        <button>{currState === "sign up" ? "Create Account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>I agree to the Terms of Service and Privacy Policy</p>
+        </div>
+        {currState === "Login" ? <p>Create a new account? <span onClick={()=>setCurrState("sign up")}>Click here</span></p>:
+        <p>Already have an account? <span onClick={()=>setCurrState("Login")} >Login here</span></p>}
+        
+        
+      </form>
+    </div>
+  )
+}
+
+export default LoginPopup
